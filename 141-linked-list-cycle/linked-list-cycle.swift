13f0ab26b70634e6c45c6da9b5abe8a3785e29d5
Atef Elsaeed (عاطف السعيد) <36440426+atefelsaeed@users.noreply.github.com>
2025-12-06 23:57:34 +0200

    Time: 18 ms (94.38%) | Memory: 18 MB (43.12%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        if head == nil || head?.next == nil {
+            return false
+        }
+
+        var slow = head
+        var fast = head
+
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+
+            if slow === fast {
+               return true
+            }
+        }
+
+        return false
+    }
+}
